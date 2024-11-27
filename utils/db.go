473e8ab0fa061c141b9f0818f2ae38366346874d
samrat-rm <samratrm2@gmail.com>
2024-11-27 14:43:56 +0530

    pq dependency added to avoid side effects and package name update

--- utils/db.go
+++ utils/db.go
@@ -2,8 +2,9 @@ package utils
 
 import (
 	"log"
-	"os"
 	"workout-tracker/models"
+
+	_ "github.com/lib/pq"
 
 	"github.com/jinzhu/gorm"
 )
@@ -12,8 +13,8 @@ var DB *gorm.DB
 
 func InitDB() {
 	var err error
-	dsn := os.Getenv("DB_DSN") // Example: "host=localhost user=postgres password=1234 dbname=workout_tracker sslmode=disable"
-	DB, err = gorm.Open("postgres", dsn)
+	// dsn := os.Getenv("DB_DSN") // Example: "host=localhost user=postgres password=1234 dbname=workout_tracker sslmode=disable"
+	DB, err = gorm.Open("postgres", "user=samrat dbname=workout_tracker sslmode=disable")
 	if err != nil {
 		log.Fatalf("Error connecting to the database: %v", err)
 	}
