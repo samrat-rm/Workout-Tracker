fd513d6349d6d69961b417afe5f0a161783b318a
samrat-rm <samratrm2@gmail.com>
2024-11-27 14:44:20 +0530

    feat: inti DB and router setup in main.go

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"workout-tracker/handlers"
+	"workout-tracker/utils"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Workout Tracker program inti...")
+	utils.InitDB()
+
+	router := mux.NewRouter()
+	router.HandleFunc("/signup", handlers.SignUp).Methods("POST")
+	router.HandleFunc("/login", handlers.Login).Methods("POST")
+	router.HandleFunc("/logout", handlers.Logout).Methods("POST")
+
+	log.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", router)
 }
