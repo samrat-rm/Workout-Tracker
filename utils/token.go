e36b0bb1713213cb23756c866278015c3e97e03e
samrat-rm <samratrm2@gmail.com>
2024-11-27 14:06:43 +0530

    feat: GenerateToken method added

---
+++ utils/token.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtSecret = []byte("your_secret_key")
+
+func GenerateToken(userID uint) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(time.Hour * 24).Unix(), // Token expires in 24 hours
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	return token.SignedString(jwtSecret)
+}
