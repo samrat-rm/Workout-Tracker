fd6c3264eafd66d618564ffa0e6730ec06813599
samrat-rm <samratrm2@gmail.com>
2024-11-28 16:34:15 +0530

    added new ProgressLog model and minor refactors

--- models/user.go
+++ models/user.go
@@ -6,8 +6,9 @@ type User struct {
 	gorm.Model
 	Username       string        `gorm:"unique" json:"username"`
 	Password       string        `json:"password"`
-	Workouts       []WorkoutPlan `json:"workouts"`
+	WorkoutPlans   []WorkoutPlan `json:"workouts"`
 	HasWorkoutPlan bool          `json:"has_workout_plan"`
+	ProgressLog    []ProgressLog `json:"progress_log"`
 }
 
 type Muscle int
@@ -32,7 +33,17 @@ const (
 	Flexibility
 )
 
+type Status int
+
+const (
+	NotStarted Status = iota
+	InProgress
+	Completed
+	Quit
+)
+
 type Exercise struct {
+	gorm.Model
 	Name               string           `json:"name"`
 	Description        string           `json:"description"`
 	Category           ExerciseCategory `json:"category"`
@@ -42,7 +53,23 @@ type Exercise struct {
 
 type WorkoutPlan struct {
 	gorm.Model
-	Exercises []Exercise `json:"exercises"`
+	UserID      uint       `json:"user_id"`
+	Description string     `json:"description"`
+	Status      Status     `json:"status"`
+	Exercises   []Exercise `json:"exercises"`
+}
+
+// TODO Add start and end date for workouts
+
+type ProgressLog struct {
+	gorm.Model
+	UserID         uint    `json:"user_id"`          // Associate the progress log with a user
+	WorkoutPlanID  uint    `json:"workout_plan_id"`  // Link progress to a specific workout plan
+	Date           string  `json:"date"`             // Log date
+	ExerciseID     uint    `json:"exercise_id"`      // Log progress for a specific exercise
+	Reps           int     `json:"reps"`             // Number of repetitions
+	Sets           int     `json:"sets"`             // Number of sets
+	DurationInMins float64 `json:"duration_in_mins"` // Duration in minutes, for cardio or flexibility exercises
 }
 
 func (m Muscle) String() string {
