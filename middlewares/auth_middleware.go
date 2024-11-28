cb4d74e59f9a1e9b7834544194cd1f46dfc93b24
samrat-rm <samratrm2@gmail.com>
2024-11-28 16:33:43 +0530

    feat: Auth middleware func updated

--- middlewares/auth_middleware.go
+++ middlewares/auth_middleware.go
@@ -12,32 +12,38 @@ var jwtSecret = []byte("your_secret_key") //!TODO check to env
 
 func JwtMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Extract the token from the Authorization header
 		tokenString := r.Header.Get("Authorization")
 		if tokenString == "" {
 			http.Error(w, "Missing token", http.StatusUnauthorized)
 			return
 		}
 
-		// Remove "Bearer " prefix
-		tokenString = strings.TrimPrefix(tokenString, "Bearer ")
-
-		// Parse and validate the token
-		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-			// Ensure the token method is HS256
-			if token.Method != jwt.SigningMethodHS256 {
-				return nil, fmt.Errorf("unexpected signing method %v", token.Header["alg"])
-			}
-			// Return the secret key for token verification
-			return jwtSecret, nil
-		})
-
+		token, err := formatTokenString(tokenString)
 		if err != nil || !token.Valid {
 			http.Error(w, "Invalid token", http.StatusUnauthorized)
 			return
 		}
-
-		// Token is valid, continue to the next handler
 		next.ServeHTTP(w, r)
 	})
+}
+
+func formatTokenString(rawToken string) (*jwt.Token, error) {
+	tokenString := strings.TrimPrefix(rawToken, "Bearer ")
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Ensure the token method is HS256
+		if token.Method != jwt.SigningMethodHS256 {
+			return nil, fmt.Errorf("unexpected signing method %v", token.Header["alg"])
+		}
+
+		return jwtSecret, nil
+	})
+	if err != nil {
+		fmt.Println("Error while parsing JWT tokens , ", err.Error())
+		return &jwt.Token{}, err
+	}
+	if !token.Valid {
+		fmt.Println("JWT Token is invalid, Please login in again")
+	}
+	return token, nil
 }
